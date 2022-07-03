f31a6fa690e53eb45e3d7a2bf80c19e6c15918cf
shekhus <48991661+shekhus@users.noreply.github.com>
2022-07-03 23:25:07 +0530

    add components -Navbar,Footer,Loader,Services,Transactions and Welcome

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,19 @@
-const App =() =>{
+// import {Navbar,Footer,Services,Transactions,Welcome,Loader} from './components'
+import {Navbar,Footer,Services,Transactions,Welcome} from './components'
 
+const App = ()=> {
   return (
-    <div className="App">
-       <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="min-h-screen">
+     <div className="gradient-bg-welcome">
+       <Navbar />
+       <Welcome />
+     </div>
+     <Services />
+     <Transactions />
+     <Footer />
     </div>
-  )
+  );
 }
+
 
 export default App
